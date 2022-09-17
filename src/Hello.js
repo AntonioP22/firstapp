390583b7d5360f8e386185b703abc4f227ccdf03
Antonio Paturzo <antoniopdev@icloud.com>
2022-09-16 20:22:41 -0700

    initial commit

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Hello extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+        <p>Welcome to React</p>
+      </div>
+    );
+  }
+}
+
+export default Hello;
